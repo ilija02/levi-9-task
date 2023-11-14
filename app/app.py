fa867b2f0b60fe0db74f6563aa93a2807bc8bf69
Ilija Radosavljevic <ikaikica.ir@gmail.com>
2023-11-14 15:24:22 +0100

    rename player_rows to player_rows_mean

--- app/app.py
+++ app/app.py
@@ -40,75 +40,75 @@ def calculate_player_stats(player_name):
     return response
 
 
-def calculate_traditional_stats(player_rows: pd.DataFrame):
+def calculate_traditional_stats(player_rows_mean: pd.DataFrame):
     traditional_stats = {}
     traditional_stats["freeThrows"] = {}
     traditional_stats["twoPoints"] = {}
     traditional_stats["threePoints"] = {}
 
-    traditional_stats["freeThrows"]["attempts"] = player_rows["FTA"].round(1)
-    traditional_stats["freeThrows"]["made"] = player_rows["FTM"].round(1)
+    traditional_stats["freeThrows"]["attempts"] = player_rows_mean["FTA"].round(1)
+    traditional_stats["freeThrows"]["made"] = player_rows_mean["FTM"].round(1)
     traditional_stats["freeThrows"]["shootingPercentage"] = (
-        player_rows["FTM"] / player_rows["FTA"] * 100
+        player_rows_mean["FTM"] / player_rows_mean["FTA"] * 100
     ).round(1)
 
-    traditional_stats["twoPoints"]["attempts"] = player_rows["2PA"].round(1)
-    traditional_stats["twoPoints"]["made"] = player_rows["2PM"].round(1)
+    traditional_stats["twoPoints"]["attempts"] = player_rows_mean["2PA"].round(1)
+    traditional_stats["twoPoints"]["made"] = player_rows_mean["2PM"].round(1)
     traditional_stats["twoPoints"]["shootingPercentage"] = (
-        player_rows["2PM"] / player_rows["2PA"] * 100
+        player_rows_mean["2PM"] / player_rows_mean["2PA"] * 100
     ).round(1)
 
-    traditional_stats["threePoints"]["attempts"] = player_rows["3PA"].round(1)
-    traditional_stats["threePoints"]["made"] = player_rows["3PM"].round(1)
+    traditional_stats["threePoints"]["attempts"] = player_rows_mean["3PA"].round(1)
+    traditional_stats["threePoints"]["made"] = player_rows_mean["3PM"].round(1)
     traditional_stats["threePoints"]["shootingPercentage"] = (
-        player_rows["3PM"] / player_rows["3PA"] * 100
+        player_rows_mean["3PM"] / player_rows_mean["3PA"] * 100
     ).round(1)
 
     traditional_stats["points"] = (
-        player_rows["FTM"]
-        + 2 * player_rows["2PM"]
-        + 3 * player_rows["3PM"]
+        player_rows_mean["FTM"]
+        + 2 * player_rows_mean["2PM"]
+        + 3 * player_rows_mean["3PM"]
     ).round(1)
-    traditional_stats["rebounds"] = player_rows["REB"].round(1)
-    traditional_stats["blocks"] = player_rows["BLK"].round(1)
-    traditional_stats["assists"] = player_rows["AST"].round(1)
-    traditional_stats["steals"] = player_rows["STL"].round(1)
-    traditional_stats["turnovers"] = player_rows["TOV"].round(1)
+    traditional_stats["rebounds"] = player_rows_mean["REB"].round(1)
+    traditional_stats["blocks"] = player_rows_mean["BLK"].round(1)
+    traditional_stats["assists"] = player_rows_mean["AST"].round(1)
+    traditional_stats["steals"] = player_rows_mean["STL"].round(1)
+    traditional_stats["turnovers"] = player_rows_mean["TOV"].round(1)
 
     return traditional_stats
 
 
-def calculate_advanced_stats(player_rows: pd.DataFrame):
+def calculate_advanced_stats(player_rows_mean: pd.DataFrame):
     advanced_stats = {}
     TPS = (
-        player_rows["FTM"]
-        + 2 * player_rows["2PM"]
-        + 3 * player_rows["3PM"]
+        player_rows_mean["FTM"]
+        + 2 * player_rows_mean["2PM"]
+        + 3 * player_rows_mean["3PM"]
     )
     advanced_stats["valorization"] = (
         (
-            player_rows["FTM"]
-            + 2 * player_rows["2PM"]
-            + 3 * player_rows["3PM"]
-            + player_rows["REB"]
-            + player_rows["BLK"]
-            + player_rows["AST"]
-            + player_rows["STL"]
+            player_rows_mean["FTM"]
+            + 2 * player_rows_mean["2PM"]
+            + 3 * player_rows_mean["3PM"]
+            + player_rows_mean["REB"]
+            + player_rows_mean["BLK"]
+            + player_rows_mean["AST"]
+            + player_rows_mean["STL"]
         )
         - (
-            player_rows["FTA"]
-            - player_rows["FTM"]
-            + player_rows["2PA"]
-            - player_rows["2PM"]
-            + player_rows["3PA"]
-            - player_rows["3PM"]
-            + player_rows["TOV"]
+            player_rows_mean["FTA"]
+            - player_rows_mean["FTM"]
+            + player_rows_mean["2PA"]
+            - player_rows_mean["2PM"]
+            + player_rows_mean["3PA"]
+            - player_rows_mean["3PM"]
+            + player_rows_mean["TOV"]
         )
     ).round(1)
 
     advanced_stats["effectiveFieldGoalPercentage"] = (
-        (player_rows["2PM"] + 1.5 * player_rows["3PM"])
-        / (player_rows["2PA"] + player_rows["3PA"])
+        (player_rows_mean["2PM"] + 1.5 * player_rows_mean["3PM"])
+        / (player_rows_mean["2PA"] + player_rows_mean["3PA"])
         * 100
     ).round(1)
 
@@ -117,22 +117,22 @@ def calculate_advanced_stats(player_rows: pd.DataFrame):
         / (
             2
             * (
-                player_rows["2PA"]
-                + player_rows["3PA"]
-                + 0.475 * player_rows["FTA"]
+                player_rows_mean["2PA"]
+                + player_rows_mean["3PA"]
+                + 0.475 * player_rows_mean["FTA"]
             )
         )
         * 100
     ).round(1)
 
     advanced_stats["hollingerAssistRatio"] = (
-        player_rows["AST"]
+        player_rows_mean["AST"]
         / (
-            player_rows["2PA"]
-            + player_rows["3PA"]
-            + 0.475 * player_rows["FTA"]
-            + player_rows["AST"]
-            + player_rows["TOV"]
+            player_rows_mean["2PA"]
+            + player_rows_mean["3PA"]
+            + 0.475 * player_rows_mean["FTA"]
+            + player_rows_mean["AST"]
+            + player_rows_mean["TOV"]
         )
         * 100
     ).round(1)
